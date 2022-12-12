719286c5a476982363af57f9f9b62a08a8ae85a5
Viet Truong <viettruong0825@gmail.com>
2022-12-12 22:53:59 +0700

    complete ui chat

--- src/components/Message/message.js
+++ src/components/Message/message.js
@@ -1,9 +1,23 @@
 import classNames from "classnames/bind";
 import styles from "./Message.module.scss";
 
+import Image from "../Images";
+
 const cx = classNames.bind(styles);
 function Message() {
-    return <div></div>;
+    return (
+        <div className={cx("wrapper-message")}>
+            <div className={cx("message", "owner")}>
+                <div className={cx("message-info")}>
+                    <Image src={""} className={cx("avatar")} />
+                    <span className={cx("message-time")}>Just now</span>
+                </div>
+                <div className={cx("message-content")}>
+                    <p className={cx("message-p")}>Hello</p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Message;
