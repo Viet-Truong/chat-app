e9a6d45cddd45d913a0361581bb3d71eb54f91c7
Viet Truong <viettruong0825@gmail.com>
2023-01-12 23:37:52 +0700

    make ui member of profile chat

--- src/components/Profile/profile.js
+++ src/components/Profile/profile.js
@@ -7,15 +7,16 @@ import { db } from "../../firebase/config";
 import Image from "../Images";
 import { ChatContext } from "./../../context/ChatContext";
 
-import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faEllipsis, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { AuthContext } from "../../context/AuthContext";
 
 const cx = classnames.bind(styles);
 function Profile() {
     const { data } = useContext(ChatContext);
-
+    const { currentUser } = useContext(AuthContext);
     const [messages, setMessages] = useState([]);
-
+    console.log(currentUser);
     useEffect(() => {
         const unSub = onSnapshot(doc(db, "chats", data.chatId), (doc) => {
             doc.exists() && setMessages(doc.data().messages);
@@ -24,6 +25,7 @@ function Profile() {
             unSub();
         };
     }, [data.chatId]);
+
     return (
         <div className={cx("wrapper")}>
             <div className={cx("close")}>
@@ -36,6 +38,33 @@ function Profile() {
                 />
                 <FontAwesomeIcon icon={""} />
                 <h2 className={cx("user-name")}>{data.user?.name}</h2>
+            </div>
+            <div className={cx("description")}>
+                <h3 className={cx("title")}>Members of Chat </h3>
+                <div className={cx("member")}>
+                    <Image
+                        src={data.user?.profile_picture}
+                        className={cx("avatar-member")}
+                    />
+                    <h2 className={cx("name-member")}>{data.user?.name}</h2>
+                    <FontAwesomeIcon
+                        icon={faEllipsis}
+                        className={cx("more-member")}
+                    />
+                </div>
+                <div className={cx("member")}>
+                    <Image
+                        src={currentUser.photoURL}
+                        className={cx("avatar-member")}
+                    />
+                    <h2 className={cx("name-member")}>
+                        {currentUser.displayName}
+                    </h2>
+                    <FontAwesomeIcon
+                        icon={faEllipsis}
+                        className={cx("more-member")}
+                    />
+                </div>
             </div>
             <div className={cx("share-media")}>
                 <h3 className={cx("title")}>Shared Media</h3>
