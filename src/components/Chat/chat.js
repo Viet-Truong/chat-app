f82487f25e77a1cfb1633a6d38c7e95db0fce02f
Viet Truong <viettruong0825@gmail.com>
2023-02-03 21:00:28 +0700

    update ui sidebar and chat info user

--- src/components/Chat/chat.js
+++ src/components/Chat/chat.js
@@ -3,19 +3,28 @@ import styles from "./Chat.module.scss";
 
 import Messages from "./../Messages/messages";
 import Input from "../Input/input";
+import Image from "../Images";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faVideo } from "@fortawesome/free-solid-svg-icons";
 import { useContext } from "react";
 import { ChatContext } from "./../../context/ChatContext";
+import Profile from "./../Profile/profile";
 
 const cx = classNames.bind(styles);
 function Chat() {
     const { data } = useContext(ChatContext);
+    console.log(data);
     return (
         <div className={cx("wrapper")}>
             <div className={cx("header")}>
-                <h4 className={cx("name-user")}>{data.user?.name}</h4>
+                <div className={cx("info")}>
+                    <Image
+                        src={data.user?.profile_picture}
+                        className={cx("profile_picture")}
+                    />
+                    <h4 className={cx("name-user")}>{data.user?.name}</h4>
+                </div>
                 <div className={cx("action")}>
                     <FontAwesomeIcon icon={faVideo} />
                 </div>
--- src/components/Sidebar/sidebar.js
+++ src/components/Sidebar/sidebar.js
@@ -4,6 +4,8 @@ import styles from "./Sidebar.module.scss";
 import Navbar from "../Navbar/navbar";
 import Search from "../Search/search";
 import ListChat from "../ListChat/listChat";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUsersRectangle } from "@fortawesome/free-solid-svg-icons";
 
 const cx = classnames.bind(styles);
 function Sidebar() {
@@ -12,6 +14,10 @@ function Sidebar() {
             <Navbar className={cx("navbar")} />
             <Search />
             <ListChat />
+            <div className={cx("newGroup")}>
+                <FontAwesomeIcon icon={faUsersRectangle} />
+                <p>Add a new group conversation</p>
+            </div>
         </div>
     );
 }
