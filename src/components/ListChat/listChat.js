13fa7cf0bfadd26b535d0538c91071c6a88eae01
Viet Truong <viettruong0825@gmail.com>
2023-06-30 22:13:55 +0700

    fix project

--- src/components/ListChat/listChat.js
+++ src/components/ListChat/listChat.js
@@ -1,12 +1,12 @@
-import classNames from "classnames/bind";
-import styles from "./ListChat.module.scss";
-import { useState, useEffect, useContext } from "react";
-import { doc, onSnapshot } from "firebase/firestore";
+import classNames from 'classnames/bind';
+import styles from './ListChat.module.scss';
+import { useState, useEffect, useContext } from 'react';
+import { doc, onSnapshot } from 'firebase/firestore';
 
-import { db } from "../../firebase/config";
-import Image from "../Images";
-import { AuthContext } from "./../../context/AuthContext";
-import { ChatContext } from "./../../context/ChatContext";
+import { db } from '../../firebase/config';
+import Image from '../Images';
+import { AuthContext } from './../../context/AuthContext';
+import { ChatContext } from './../../context/ChatContext';
 
 const cx = classNames.bind(styles);
 function ListChat() {
@@ -17,7 +17,7 @@ function ListChat() {
     useEffect(() => {
         const getChats = () => {
             const unSub = onSnapshot(
-                doc(db, "userChats", currentUser.uid),
+                doc(db, 'userChats', currentUser.uid),
                 (doc) => {
                     setChats(doc.data());
                 }
@@ -30,30 +30,30 @@ function ListChat() {
     }, [currentUser.uid]);
 
     const handleSelect = (u) => {
-        dispatch({ type: "CHANGE_USER", payload: u });
+        dispatch({ type: 'CHANGE_USER', payload: u });
     };
     return (
-        <div className={cx("wrapper")}>
+        <div className={cx('wrapper')}>
             {/* use sort to sort user chat recently */}
             {Object.entries(chats)
                 ?.sort((a, b) => b[1].date - a[1].date)
                 .map((chat) => (
                     <div
-                        className={cx("user-chat")}
+                        className={cx('user-chat')}
                         key={chat[0]}
                         onClick={() => handleSelect(chat[1].userInfo)}
                     >
-                        <div className={cx("user-avatar")}>
+                        <div className={cx('user-avatar')}>
                             <Image
                                 src={chat[1].userInfo.profile_picture}
-                                className={cx("avatar")}
+                                className={cx('avatar')}
                             />
                         </div>
-                        <div className={cx("user-chat-info")}>
-                            <p className={cx("user-name")}>
+                        <div className={cx('user-chat-info')}>
+                            <p className={cx('user-name')}>
                                 {chat[1].userInfo.name}
                             </p>
-                            <p className={cx("user-last-chat")}>
+                            <p className={cx('user-last-chat')}>
                                 {chat[1].lastMessage?.text}
                             </p>
                         </div>
